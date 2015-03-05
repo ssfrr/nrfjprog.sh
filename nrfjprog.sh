6489e354b7153cdd235cf9f5d247057283f97826
Spencer Russell <spencer.f.russell@gmail.com>
2015-03-04 19:28:50 -0500

    adds --rtt command and colorizes output

--- nrfjprog.sh
+++ nrfjprog.sh
@@ -8,7 +8,7 @@ which relies on JLinkExe to interface with the JLink hardware.
 
 usage:
 
-nrfjprog.sh action hexfile
+nrfjprog.sh <action> [hexfile]
 
 where action is one of
   --reset
@@ -16,8 +16,13 @@ where action is one of
   --erase-all
   --flash
   --flash-softdevice
+  --rtt
 
 EOF
+
+GREEN="\033[32m"
+RESET="\033[0m"
+STATUS_COLOR=$GREEN
 
 TOOLCHAIN_PREFIX=arm-none-eabi
 # assume the tools are on the system path
@@ -41,8 +46,7 @@ TMPSCRIPT=/tmp/tmp_$$.jlink
 TMPBIN=/tmp/tmp_$$.bin
 if [ "$1" = "--reset" ]; then
     echo ""
-    echo "resetting..."
-    echo "------------"
+    echo -e "${STATUS_COLOR}resetting...${RESET}"
     echo ""
     echo "r" > $TMPSCRIPT
     echo "g" >> $TMPSCRIPT
@@ -50,7 +54,9 @@ if [ "$1" = "--reset" ]; then
     $JLINK $TMPSCRIPT
     rm $TMPSCRIPT
 elif [ "$1" = "--pin-reset" ]; then
-    echo "resetting with pin..."
+    echo ""
+    echo -e "${STATUS_COLOR}resetting with pin...${RESET}"
+    echo ""
     echo "w4 40000544 1" > $TMPSCRIPT
     echo "r" >> $TMPSCRIPT
     echo "exit" >> $TMPSCRIPT
@@ -58,8 +64,7 @@ elif [ "$1" = "--pin-reset" ]; then
     rm $TMPSCRIPT
 elif [ "$1" = "--erase-all" ]; then
     echo ""
-    echo "perfoming full erase..."
-    echo "-----------------------"
+    echo -e "${STATUS_COLOR}perfoming full erase...${RESET}"
     echo ""
     echo "w4 4001e504 2" > $TMPSCRIPT
     echo "w4 4001e50c 1" >> $TMPSCRIPT
@@ -70,8 +75,7 @@ elif [ "$1" = "--erase-all" ]; then
     rm $TMPSCRIPT
 elif [ "$1" = "--flash" ]; then
     echo ""
-    echo "flashing $BIN..."
-    echo "------------------------------------------"
+    echo -e "${STATUS_COLOR}flashing ${BIN}...${RESET}"
     echo ""
     FLASH_START_ADDRESS=`$OBJDUMP -h $OUT -j .text | grep .text | awk '{print $4}'`
     echo "r" > $TMPSCRIPT
@@ -83,8 +87,7 @@ elif [ "$1" = "--flash" ]; then
     rm $TMPSCRIPT
 elif [ "$1" = "--flash-softdevice" ]; then
     echo ""
-    echo "flashing softdevice $HEX..."
-    echo "------------------------------------------"
+    echo -e "${STATUS_COLOR}flashing softdevice ${HEX}...${RESET}"
     echo ""
     $OBJCOPY -Iihex -Obinary $HEX $TMPBIN
     # Write to NVMC to enable erase, do erase all, wait for completion. reset
@@ -101,6 +104,22 @@ elif [ "$1" = "--flash-softdevice" ]; then
     $JLINK $TMPSCRIPT
     rm $TMPSCRIPT
     rm $TMPBIN
+elif [ "$1" = "--rtt" ]; then
+    # trap the SIGINT signal so we can clean up if the user CTRL-C's out of the
+    # RTT client
+    trap ctrl_c INT
+    function ctrl_c() {
+        return
+    }
+    echo -e "${STATUS_COLOR}Starting RTT Server...${RESET}"
+    JLinkExe -device nrf51822 -if swd -speed 1000 &
+    JLINK_PID=$!
+    sleep 1
+    echo -e "\n${STATUS_COLOR}Connecting to RTT Server...${RESET}"
+    #telnet localhost 19021
+    JLinkRTTClient
+    echo -e "\n${STATUS_COLOR}Killing RTT server ($JLINK_PID)...${RESET}"
+    kill $JLINK_PID
 else
     echo "$USAGE"
 fi
