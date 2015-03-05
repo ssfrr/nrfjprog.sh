bfbd1f1ceae7ad20af2242c786bad5ecbed69cac
Spencer Russell <spencer.f.russell@gmail.com>
2015-03-04 20:48:52 -0500

    switches to using hexfiles insteadof relying on bin and out files in the same dir

--- nrfjprog.sh
+++ nrfjprog.sh
@@ -27,23 +27,17 @@ STATUS_COLOR=$GREEN
 TOOLCHAIN_PREFIX=arm-none-eabi
 # assume the tools are on the system path
 TOOLCHAIN_PATH=
-OBJCOPY=$TOOLCHAIN_PATH$TOOLCHAIN_PREFIX-objcopy
-OBJDUMP=$TOOLCHAIN_PATH$TOOLCHAIN_PREFIX-objdump
 JLINK_OPTIONS="-device nrf51822 -if swd -speed 1000"
 
 HEX=$2
 
-# assume there's an out and bin file next to the hexfile
-OUT=${HEX/.hex/.out}
-BIN=${HEX/.hex/.bin}
-
 JLINK="JLinkExe $JLINK_OPTIONS"
 JLINKGDBSERVER="JLinkGDBServer $JLINK_OPTIONS"
 
-# the script commands come from Makefile.posix, distributed with nrf51-pure-gcc
+# the script commands come from Makefile.posix, distributed with
+# nrf51-pure-gcc. I've made some changes to use hexfiles instead of binfiles
 
 TMPSCRIPT=/tmp/tmp_$$.jlink
-TMPBIN=/tmp/tmp_$$.bin
 if [ "$1" = "--reset" ]; then
     echo ""
     echo -e "${STATUS_COLOR}resetting...${RESET}"
@@ -75,11 +69,10 @@ elif [ "$1" = "--erase-all" ]; then
     rm $TMPSCRIPT
 elif [ "$1" = "--flash" ]; then
     echo ""
-    echo -e "${STATUS_COLOR}flashing ${BIN}...${RESET}"
+    echo -e "${STATUS_COLOR}flashing ${HEX}...${RESET}"
     echo ""
-    FLASH_START_ADDRESS=`$OBJDUMP -h $OUT -j .text | grep .text | awk '{print $4}'`
     echo "r" > $TMPSCRIPT
-    echo "loadbin $BIN $FLASH_START_ADDRESS" >> $TMPSCRIPT
+    echo "loadfile $HEX" >> $TMPSCRIPT
     echo "r" >> $TMPSCRIPT
     echo "g" >> $TMPSCRIPT
     echo "exit" >> $TMPSCRIPT
@@ -89,7 +82,6 @@ elif [ "$1" = "--flash-softdevice" ]; then
     echo ""
     echo -e "${STATUS_COLOR}flashing softdevice ${HEX}...${RESET}"
     echo ""
-    $OBJCOPY -Iihex -Obinary $HEX $TMPBIN
     # Write to NVMC to enable erase, do erase all, wait for completion. reset
     echo "w4 4001e504 2" > $TMPSCRIPT
     echo "w4 4001e50c 1" >> $TMPSCRIPT
@@ -97,13 +89,12 @@ elif [ "$1" = "--flash-softdevice" ]; then
     echo "r" >> $TMPSCRIPT
     # Write to NVMC to enable write. Write mainpart, write UICR. Assumes device is erased.
     echo "w4 4001e504 1" > $TMPSCRIPT
-    echo "loadbin $TMPBIN 0" >> $TMPSCRIPT
+    echo "loadfile $HEX" >> $TMPSCRIPT
     echo "r" >> $TMPSCRIPT
     echo "g" >> $TMPSCRIPT
     echo "exit" >> $TMPSCRIPT
     $JLINK $TMPSCRIPT
     rm $TMPSCRIPT
-    rm $TMPBIN
 elif [ "$1" = "--rtt" ]; then
     # trap the SIGINT signal so we can clean up if the user CTRL-C's out of the
     # RTT client
